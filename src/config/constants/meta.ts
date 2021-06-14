d6669421b8137badad5052dde6c67af1557f8348
bunnymoondev <bunnymoon.finance@gmail.com>
2021-06-14 10:21:28 +0900

    typo

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -3,8 +3,8 @@ import { PageMeta } from './types'
 export const DEFAULT_META: PageMeta = {
   title: 'BunnyMoonFarm',
   description:
-    'The most popular AMM on BSC by user count! Earn BUNNYMOON through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by BunnyMoonFarm), NFTs, and more, on a platform you can trust.',
-  image: 'https://bunnymoonfarm.web.app/images/hero.png',
+    'Earn BUNNYMOON through yield farming or win it in the Lottery, then stake it in Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by BunnyMoonFarm), NFTs, and more, on a platform you can trust.',
+  image: 'https://bunnymoonfarm.web.app/images/hero2.png',
 }
 
 export const customMeta: { [key: string]: PageMeta } = {
